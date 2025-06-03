e2613ede45bfb5c3810573bb7970dc892ae8b8c8
zeelrupapara <zeelrupapara@gmail.com>
2025-06-03 11:48:19 +0530

    feat: nats setup in grpc init

--- cmd/grpc.go
+++ cmd/grpc.go
@@ -5,6 +5,7 @@ import (
 	"gitlab.com/zeelrupapara/trade-engine/config"
 	database "gitlab.com/zeelrupapara/trade-engine/database/migrations"
 	grpc_server "gitlab.com/zeelrupapara/trade-engine/pkg/grpc"
+	"gitlab.com/zeelrupapara/trade-engine/pkg/nats"
 	"go.uber.org/zap"
 )
 
@@ -18,12 +19,19 @@ func GetAPICommandDef(cfg config.AppConfig, logger *zap.Logger) cobra.Command {
 			// Create DB connection
 			db, err := database.Connect(cfg.DB)
 			if err != nil {
-				logger.Error(err.Error())
+				logger.Error(err.Error(), zap.Any("Setup", "Init DB Connection"))
+				return err
+			}
+
+			// Create Message Broker connection
+			nats, err := nats.NewMsgBrokerClient(cfg)
+			if err != nil {
+				logger.Error(err.Error(), zap.Any("Setup", "Init Nats Connection"))
 				return err
 			}
 
 			// Create grpc server
-			rpcServer := grpc_server.NewGRPCServer(cfg.Port, logger, db)
+			rpcServer := grpc_server.NewGRPCServer(cfg.Port, logger, db, nats.Nc)
 			if err := rpcServer.Run(); err != nil {
 				logger.Error(err.Error())
 				return err
